ad49e0fb79671f2b4ec2f1fe33fefa4ebf706956
SHAROOQUE <mohammed.sharooque@accenture.com>
2021-07-14 19:13:52 +0530

    All FIle

---
+++ FileSystem.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+
+let TimeStamp = +new Date();
+
+const http = require("http");
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text-html" });
+    const Path =
+      "C:/Users/mohammed.sharooque/Documents/Jquery/Nodejs/FileSystem/Path";
+
+    fs.writeFile("./Path/date-time.txt", TimeStamp.toString(), (err) => {
+      if (err) throw err;
+      console.log("File Created");
+    });
+
+    fs.readdir(Path, function (err, files) {
+      console.log(typeof files);
+      res.write("The list of files in Path \n");
+      res.write(`${files.map((arr) => `${arr}`)}`);
+      console.log(files);
+      res.end();
+    });
+  })
+  .listen(3000);
